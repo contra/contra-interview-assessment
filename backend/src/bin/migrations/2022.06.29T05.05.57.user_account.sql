6783010afa9a6048cdcb27ee2f779712dd82f32d
Joseph Hall <jhallpilot@gmail.com>
2022-06-29 01:18:34 -0400

    feat: add basic user_account migration

---
+++ backend/src/bin/migrations/2022.06.29T05.05.57.user_account.sql
@@ -0,0 +1,9 @@
+CREATE TABLE user_account (
+  id PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  given_name text NOT NULL,
+  family_name text NOT NULL,
+  email_address text NOT NULL,
+  created_at timestamp with time zone DEFAULT NOW(),
+  updated_at timestamp with time zone DEFAULT NOW()
+);
+
