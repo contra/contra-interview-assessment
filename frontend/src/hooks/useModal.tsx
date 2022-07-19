9e56ad5f8a78a1bc44143604d9fda80f226c5378
Joao Gameiro <joao@podchaser.com>
2022-07-19 09:26:43 +0100

    Clean up

--- frontend/src/hooks/useModal.tsx
+++ frontend/src/hooks/useModal.tsx
@@ -3,11 +3,11 @@ import { useContext } from 'react';
 import { MODAL_TYPES } from '../Container/ModalContainer';
 
 export default () => {
-  const { modalType, setModalType, isOpen } = useContext(ModalContext);
+  const { setModalType, isOpen } = useContext(ModalContext);
 
   function showSimpleModal() {
     setModalType(MODAL_TYPES.SIMPLE);
   }
 
-  return { modalType, setModalType, isOpen, showSimpleModal };
+  return { setModalType, isOpen, showSimpleModal };
 };
