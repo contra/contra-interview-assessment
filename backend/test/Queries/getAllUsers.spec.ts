371949f5e29ac4026ddc4631c71dfaa48d2a56eb
bertolo1988 <tiagobertolo@gmail.com>
2022-08-02 18:49:40 +0100

    Add test to expect featureFlags for each user.

--- backend/test/Queries/getAllUsers.spec.ts
+++ backend/test/Queries/getAllUsers.spec.ts
@@ -25,4 +25,28 @@ describe('getAllUsers', () => {
     const usersList = response.body.data.getAllUsers;
     expect(Array.isArray(usersList)).toBe(true);
   });
+
+  it('expect featureFlags to be defined as an array for each user', async () => {
+    const queryData = {
+      query: `query Query {
+        getAllUsers {
+          id
+          featureFlags {
+            id
+            userId
+          }
+        }
+      }`,
+    };
+    const response = await request(testServer.server)
+      .post('/graphql')
+      .send(queryData);
+
+    const usersList = response.body.data.getAllUsers;
+
+    for (let user of usersList) {
+      expect(user.featureFlags).toBeDefined();
+      expect(Array.isArray(user.featureFlags)).toBe(true);
+    }
+  });
 });
