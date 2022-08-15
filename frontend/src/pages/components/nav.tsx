4348f05219b1c1d496563a22a67dff5ed4a07ceb
SKallDay <shaistakirmani@gmail.com>
2022-08-15 19:05:50 -0400

    add nav

---
+++ frontend/src/pages/components/nav.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable func-style */
+// import { useEffect } from 'react';
+
+import Link from 'next/link';
+
+type navItem = {
+  name: string;
+  path: string;
+};
+
+export type NavigationProps = {
+  navLinks: navItem[];
+};
+
+const Navigation = ({ navLinks }: NavigationProps) => {
+  return (
+    <nav className="nav">
+      {navLinks.map((link: navItem) => (
+        <Link href={link.path} key={link.name + 1}>
+          <a
+            style={{
+              color: 'black',
+              fontSize: 20,
+              textDecoration: 'none',
+            }}
+          >
+            {link.name}
+          </a>
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export { Navigation };
