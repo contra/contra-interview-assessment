78fcca563219a5ed2fe495190575086e6d15d34c
shah62 <52537686+shah62@users.noreply.github.com>
2022-08-21 15:40:33 -0400

    chore: setup stitches with custom theme

---
+++ frontend/src/stitches/index.ts
@@ -0,0 +1,6 @@
+import { createStitches } from '@stitches/react';
+import { colors } from '@/theme';
+
+export const { styled, css, globalCss, keyframes } = createStitches({
+  theme: { colors },
+});
