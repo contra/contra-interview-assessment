4a1100ea68a64f138f1ad52519aaf934a0113809
shah62 <52537686+shah62@users.noreply.github.com>
2022-08-21 16:13:29 -0400

    chore: export getCssText function from stitches config for ssr support

--- frontend/src/stitches/index.ts
+++ frontend/src/stitches/index.ts
@@ -1,6 +1,8 @@
 import { createStitches } from '@stitches/react';
 import { colors } from '@/theme';
 
-export const { styled, css, globalCss, keyframes } = createStitches({
-  theme: { colors },
-});
+export const { styled, css, getCssText, globalCss, keyframes } = createStitches(
+  {
+    theme: { colors },
+  }
+);
