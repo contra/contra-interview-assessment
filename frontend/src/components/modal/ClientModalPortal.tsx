3ea44d9083e0675eeea920a1e4af211c2a60b2d5
Johnson Tinuoye <johnsontinuoye@gmail.com>
2022-09-04 08:59:30 +0100

    Set up portal for modal component
    This includes the implementation of portal to contain modals at the end of the document body. It also contains an implementation of ClientModalPortal that ensures that modals are rendered without errors on server-side rendered routes

---
+++ frontend/src/components/modal/ClientModalPortal.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useRef, useState, type ReactNode, type ReactPortal } from "react";
+import { createPortal } from "react-dom";
+
+// For server-side rendered pages, using createPortal would cause an error because the modal container is not mounted yet.
+// By using this component, we can detect when the modal container is mounted before rendering the modal.
+
+type IClientPortal = {
+    children: ReactNode;
+}
+
+const ClientModalPortal = ({ children }: IClientPortal): ReactPortal | null => {
+    const ref = useRef<Element | null>();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        ref.current = document.querySelector("#modal");
+        setMounted(true);
+    }, []);
+
+    return ref.current && mounted ? createPortal(children, ref.current) : null;
+}
+
+export default ClientModalPortal;
--- frontend/src/components/modal/Modal.tsx
+++ frontend/src/components/modal/Modal.tsx
@@ -1,5 +1,12 @@
-const Modal = () => {
-  return <> </>;
+import { type ReactNode } from 'react';
+import ClientModalPortal from './ClientModalPortal';
+
+type IModal = {
+    children: ReactNode | ReactNode[];
+}
+
+const Modal = ({ children }: IModal) => {
+    return <ClientModalPortal>{children}</ClientModalPortal>;
 };
 
 export default Modal;
