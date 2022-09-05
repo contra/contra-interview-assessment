95b301b38367f2bfcba8995801e4d5d9add45470
murimichege <chegeeddie@gmail.com>
2022-09-05 14:37:12 +0300

    frontend app cleanup

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,20 +1,10 @@
 /* eslint-disable canonical/filename-match-exported */
 import { type NextPage } from 'next';
-import { Box } from '@mui/material'
 import TabNavigation from '@/components/Tabs/TabNavigations';
 const Index: NextPage = () => {
   return (
-    <Box sx={{
-      width:{
-        xs:300,
-        sm:200,
-        md:500,
-        xl:500
-      }
-    
-    }}>
-      <TabNavigation /> 
-         </Box>
+    <TabNavigation />
+
   )
 }
 
