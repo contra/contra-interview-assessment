0e0e37422f7a57b4c175d4315e64d2fd35b6396f
pierreh <pierrehavelaar@gmail.com>
2023-03-30 19:21:39 +0100

    Fully working frontend assesment: a custom Modal component

---
+++ frontend/src/components/Modal.tsx
@@ -0,0 +1,129 @@
+/* eslint-disable unicorn/numeric-separators-style */
+/* eslint-disable jsx-a11y/no-noninteractive-tabindex */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import FocusTrap from 'focus-trap-react';
+import React, { useContext, useEffect, useRef } from 'react';
+import Button from './Button';
+
+/**
+ * TODO:
+ * 1) create Modal component and display this on the index page ✅
+ * 2) add toggle button on index page to display the modal ✅
+ * 3) add prop to show Modal ✅
+ * 4) add a way to close Modal ✅
+ * 5)
+ * 6) add overlay ✅
+ * 7) prevent scrolling ✅
+ * 8) Handle stacking ✅
+ * 8.1) Fix ESC handling on nested dialogs ✅
+ * 8.2) Fix scroll disabling on nested dialogs ✅
+ * 9) handle focus management and tabbing ✅
+ * 10) Allow escape to close the modal ✅
+ * 11.1) Fix mobile vertical alignment for modal ✅
+ * bonus 1) add modal animation ✅
+ * bonus 2) add button animation ✅
+ */
+
+
+type Props = {
+    children: React.ReactNode,
+    handleClose: CloseHandler, 
+    show: boolean,
+    title: string,
+}
+
+type CloseHandler = () => void;
+
+/**
+ * Create overlay component that darkens the background and handles clicks
+ */
+const Overlay = ({ handleClose, zIndex }: { handleClose: CloseHandler, zIndex: number }) => {
+
+    return <div 
+        aria-hidden="true"
+        className="absolute top-0 left-0 w-full h-full bg-slate-300/80"
+        onClick={handleClose}
+        role="button" 
+        style={{zIndex}}
+        tabIndex={0}
+    />;
+}
+
+/**
+ * We are using a React context to keep track of the nesting level of modals.
+ * Then we can use this information to only disable/enable scrolling once for the topmost Modal.
+ */
+const NestingLevel = React.createContext(0);
+
+const Modal = ({ children, handleClose, show, title }: Props) => {
+
+    const nestingLevel = useContext(NestingLevel);
+    
+    const handleKey = (event: React.KeyboardEvent) => {
+        if (event.key === 'Escape' || event.key === 'Esc' || event.keyCode === 27) {
+            // without this parent modals will also close
+            event.stopPropagation();
+            handleClose();
+        }
+    }
+
+    /**
+     * Prevent background from scrolling while the Modal is open
+     */
+    // eslint-disable-next-line consistent-return
+    useEffect(() => {
+        if (nestingLevel === 0) {
+            if (show) {
+                document.body.style.overflow = 'hidden';
+            }
+            else {
+                document.body.style.overflow = 'unset';
+            }
+
+            return () => { 
+                document.body.style.overflow = 'unset';
+            };
+        }
+    }, [show, nestingLevel]);
+
+    /**
+     * Trigger animation to drop the dialog from the top
+     */
+    const dialogRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        if (dialogRef.current) {
+            dialogRef.current.classList.remove('mt-[-1000px]');
+            dialogRef.current.classList.add('mt-0');
+        }
+    });
+
+    const focusTrapOptions = {
+        clickOutsideDeactivates: true,
+        // we need escapeDeactivates to be false otherwise nested dialogs will close in the wrong order
+        escapeDeactivates: false,
+    }
+
+    return show ? (<div className="fixed top-0 left-0 w-full h-full">
+        <Overlay handleClose={handleClose} zIndex={1000+nestingLevel*2}/>
+        <FocusTrap focusTrapOptions={focusTrapOptions}>
+            <div aria-label={title}
+                className="absolute w-full transition-all duration-300 ease-out mt-[-1000px] sm:w-[400px] sm:-ml-[200px] bg-white sm:rounded-xl shadow-xl top-1/4 sm:left-1/2"
+                onKeyUp={handleKey} ref={dialogRef} role="dialog"
+                style={{zIndex: 1001+nestingLevel*2}} tabIndex={0}
+            >
+                <h2 className="text-center bg-violet-700 sm:rounded-t-xl text-white py-1">{title}</h2>
+                
+                <div className="py-8 px-8 space-y-2 sm:py-4 flex items-center flex-col sm:space-y-0 sm:space-x-6">
+                <NestingLevel.Provider value={nestingLevel+1}>
+                    {children}
+                </NestingLevel.Provider>
+                </div>
+                <div className="text-center mb-4">
+                    <Button onClick={handleClose}>Close</Button>
+                </div>
+            </div>
+        </FocusTrap>
+    </div>) : null;
+}
+
+export default Modal;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,30 @@
 /* eslint-disable canonical/filename-match-exported */
 import { type NextPage } from 'next';
+import { useState } from 'react';
+import Button from '@/components/Button';
+import Modal from '@/components/Modal';
 
 const Index: NextPage = () => {
-  return <h1>Welcome to Contra!</h1>;
+  const [showModal, setShowModal] = useState(false);
+  const [showModal2, setShowModal2] = useState(false);
+
+  return (
+    <div className="container mx-auto font-sans bg-white sm:rounded sm:mt-2 p-4">
+      <h1 className="text-center">Custom Modal Demo</h1>
+      <div className="mb-4">
+        <p className="mb-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lobortis, nulla at sollicitudin pharetra, enim quam lacinia eros, nec vehicula metus felis non lacus. Maecenas bibendum facilisis lorem at consequat. Nam tempus eleifend interdum. Nulla tincidunt orci nec velit tempor mollis. Mauris nec nisi ligula. Ut quis accumsan tortor, sit amet pulvinar dui. Sed condimentum in lorem sed lobortis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus dapibus vel lorem sit amet eleifend. Donec non congue magna. Donec egestas aliquam varius. Quisque ac eleifend nisi. Nulla dolor ipsum, pretium eget sapien non, venenatis fringilla massa. Sed sed elit ante. Sed vel lectus nec arcu porttitor accumsan in at eros.</p>
+        <p className="mb-10">Suspendisse potenti. In ut gravida purus. Fusce bibendum vitae mi id elementum. Nulla ultricies eu lectus sed semper. Maecenas nec vestibulum ligula, vitae varius augue. Morbi sit amet hendrerit ex. In ac neque in nulla sollicitudin rutrum non id dui. Fusce bibendum ligula in dolor accumsan volutpat. Sed vestibulum nisi rutrum, accumsan felis accumsan, sagittis dolor. Nullam feugiat, diam nec consequat commodo, dolor urna egestas purus, vel lobortis ante elit a purus. Nulla euismod at odio ut gravida. Suspendisse fringilla eget est at rhoncus. Nam sodales porta varius. Vivamus id massa vel nisi finibus pulvinar et a urna. Aliquam sagittis in justo sit amet dignissim.</p>
+        <p className="mb-10">Cras dictum sit amet mi vitae cursus. Nam sodales, tellus at gravida malesuada, sapien est tincidunt massa, nec tempus nibh tortor et urna. Sed vitae leo sapien. Aenean commodo, mauris vel lobortis pharetra, magna ex dignissim lorem, sed feugiat felis tortor sit amet sapien. Mauris dignissim tempor tristique. Donec sit amet enim tellus. In tincidunt ante pulvinar, dictum metus ac, cursus sapien. Quisque et faucibus odio. Sed hendrerit magna at euismod luctus. Aenean lacinia euismod est dapibus viverra. Praesent commodo nisl id sapien semper sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam vulputate diam at vulputate pulvinar. Praesent tincidunt orci ut nulla mattis tincidunt. Phasellus mattis, massa id blandit pulvinar, tortor ante tincidunt sapien, in accumsan ipsum felis sit amet felis. In dictum nulla sed elit imperdiet feugiat quis sed est.</p>
+      </div>
+      {showModal || <Button onClick={() => setShowModal(true)}>Show Modal</Button>}
+      <Modal handleClose={() => setShowModal(false)} show={showModal} title="Modal 1">
+        <Button onClick={() => setShowModal2(true)} type="secondary">Login</Button>
+        <Modal handleClose={() => setShowModal2(false)} show={showModal2} title="Modal 2">
+          Nested Modal
+        </Modal>
+      </Modal>
+    </div>
+  );
 };
 
 export default Index;
