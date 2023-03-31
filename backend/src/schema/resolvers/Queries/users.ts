bae443a40ba7c42db31dab058026f1e1512021d7
pierreh <pierrehavelaar@gmail.com>
2023-03-31 22:41:48 +0100

    Fix indent

--- backend/src/schema/resolvers/Queries/users.ts
+++ backend/src/schema/resolvers/Queries/users.ts
@@ -2,13 +2,13 @@ import { sql } from 'slonik';
 import { QueryResolvers, User } from '../../../generated/types';
 
 export const users: QueryResolvers['users'] = async (
-  _parent,
-  { environment },
-  { pool },
+	_parent,
+	{ environment },
+	{ pool },
 ) => {
-  
-  return pool.any<User>(
-    sql`SELECT id, given_name, family_name, email_address, created_at, updated_at, ${environment} as environment FROM user_account;`
-  );
+	
+	return pool.any<User>(
+		sql`SELECT id, given_name, family_name, email_address, created_at, updated_at, ${environment} as environment FROM user_account;`
+	);
 
 };
