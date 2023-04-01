c69c1011d509cea19dd06c4414b194c125d538ff
Vijay Rangan <vijay.rangan@gmail.com>
2023-04-01 20:25:46 +0200

    Create custom error formatter
    1. Inject repository via context
    2. Use custom error handler to hide exception details from clients

--- backend/src/factories/createFastifyServer.ts
+++ backend/src/factories/createFastifyServer.ts
@@ -2,14 +2,19 @@ import { ApolloServer } from 'apollo-server-fastify';
 import fastify from 'fastify';
 import { importSchema } from 'graphql-import';
 import { makeExecutableSchema } from 'graphql-tools';
-import { Model } from 'objection';
 import path from 'path';
 // @ts-ignore
 import type { Knex } from 'knex';
+import Logger from 'roarr';
+import { v4 } from 'uuid';
+import { Repository } from '../bin/repository';
 import { resolvers } from '../schema/resolvers';
 
+const logger = Logger.child({ context: 'factories/createFastifyServer' });
+
 export const createFastifyServer = async (pool: Knex) => {
-  Model.knex(pool);
+  
+
   const executableSchema = makeExecutableSchema({
     inheritResolversFromInterfaces: true,
     resolvers,
@@ -18,14 +23,35 @@ export const createFastifyServer = async (pool: Knex) => {
   });
 
   const app = fastify();
+  const repository = new Repository();
 
   const graphQLServer = new ApolloServer({
     context: ({ request, reply }) => ({
       pool,
       reply,
       request,
+      repository,
+      correlationId: request.headers['x-correlation-id'] 
+        ? request.headers['x-correlation-id']
+        : v4()
     }),
     schema: executableSchema,
+    formatError: (error) => {
+      logger.error(error, 'Error handling request');
+
+      if (error.extensions.exception && error.extensions.exception.name === 'UniqueViolationError') {
+        const { detail } = error.extensions.exception.nativeError;
+        const [_, featureFlagId, userId] = detail.match(/(\d{1}),\s(\d{1})/);
+
+        throw new Error(`User with ID ${userId} already targetted with feature flag ID ${featureFlagId}. Cannot re-assign.`);
+      }
+
+      if (error.extensions.exception && error.extensions.exception.client === 'postgres') {
+        throw new Error('Internal server error');
+      }
+
+      return error;
+    },
   });
 
   await graphQLServer.start();
