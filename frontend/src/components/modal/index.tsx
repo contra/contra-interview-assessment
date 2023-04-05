1164b077ef6b4aaf468b764e52882e2d85d25fc0
vi-alt-hidden (aka Vinicius) <>
2023-04-05 19:46:29 -0300

    feat: isOpen prop

--- frontend/src/components/modal/index.tsx
+++ frontend/src/components/modal/index.tsx
@@ -1,3 +1,13 @@
-export const Modal = () => {
+export type ModalProps = {
+  isOpen: boolean;
+};
+
+export const Modal = (props: ModalProps) => {
+  const { isOpen } = props;
+
+  if (!isOpen) {
+    return null;
+  }
+
   return <div>I'm a modal, I swear!</div>;
 };
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,18 @@
 /* eslint-disable canonical/filename-match-exported */
 import { type NextPage } from 'next';
+import { useState } from 'react';
 import { Modal } from '@/components/modal';
 
 const Index: NextPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <div>
       <h1>Welcome to Contra!</h1>
-      <Modal />
+      <button onClick={() => setIsModalOpen(true)} type="button">
+        Show modal
+      </button>
+      <Modal isOpen={isModalOpen} />
     </div>
   );
 };
