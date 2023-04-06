45b30d2ce8239678ce773c42e4eb361742e7eefb
vi-alt-hidden (aka Vinicius) <>
2023-04-05 22:35:16 -0300

    test: simplify jest config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -11,7 +11,7 @@ const customJestConfig = {
   // Add more setup options before each test is run
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
   testEnvironment: 'jest-environment-jsdom',
 };
 
