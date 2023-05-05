a2f3d6b2c36c3bf72aa5539f80ddbada7d6be394
Eduardo Zamora <eduzambl@gmail.com>
2023-05-04 23:07:22 -0600

    feat: include css on main app

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,6 @@
-/* eslint-disable canonical/filename-match-exported */
+/* eslint-disable canonical/filename-match-exported, import/no-unassigned-import */
 import { type AppProps } from 'next/app';
+import './globals.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
