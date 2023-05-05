c81653d0b4021a04cecf4eac47986028773289aa
Eduardo Zamora <eduzambl@gmail.com>
2023-05-05 00:02:45 -0600

    feat: example implementation and usage

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,40 @@
 /* eslint-disable canonical/filename-match-exported */
 import { type NextPage } from 'next';
+import { useState } from 'react';
+import { Modal } from '@/components/Modal';
 
 const Index: NextPage = () => {
-  return <h1>Welcome to Contra!</h1>;
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
+
+  return (
+    <div className="p-4">
+      <h1>Welcome to Contra!</h1>
+      <div className="container mx-auto px-4 py-8">
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+          onClick={openModal}
+          type="button"
+        >
+          Open Modal
+        </button>
+
+        <Modal isOpen={modalOpen} onClose={closeModal}>
+          <h2 className="text-lg font-bold mb-4">Modal Content</h2>
+          <p className="mb-4">This is the content inside the modal.</p>
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded"
+            onClick={closeModal}
+            type="button"
+          >
+            Close Modal
+          </button>
+        </Modal>
+      </div>
+    </div>
+  );
 };
 
 export default Index;
