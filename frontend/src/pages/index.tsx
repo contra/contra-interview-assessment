89d5efbe2bf37cabd7c9abed829f6e8eea1957a4
Lucas Freitas <lucas@lucasfreitas.com.br>
2023-07-29 00:27:25 +0200

    Added .editorconfig to force spaces on my editor, fixed spelling and added a Modal component that supports stacking. console.log are left intentionally for reviewer to look into thought process

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,20 @@
 /* eslint-disable canonical/filename-match-exported */
 import { type NextPage } from 'next';
+import {Modal} from "@/components/Modal";
 
 const Index: NextPage = () => {
-  return <h1>Welcome to Contra!</h1>;
+  return (
+    <>
+      <h1>Welcome to Contra!</h1>
+      {/* eslint-disable-next-line no-console */}
+      <Modal closeText="Close" isOpen={true} onClose={() => console.log('fechei o 1')}>
+        <p>This is a test.</p>
+      </Modal>
+      <Modal closeText="Close" isOpen={true} onClose={() => console.log('fechei o 2')}>
+        <p>This is a second test.</p>
+      </Modal>
+    </>
+  );
 };
 
 export default Index;
